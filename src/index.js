17c39743514263f9be8135721dc0dfbf39f4fcb9
Mary West <westmarye1219@gmail.com>
2018-11-03 10:49:45 -0500

    added gifs

--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@ import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
 
-$('dogs').show();
+import getCats from './catGetter';
+
+const createCatCards = (cats) => {
+  let newString = '';
+  cats.forEach((cat) => {
+    newString += `
+        <div class="card u-clearfix">
+          <div class="card-body">
+            <span class="card-number card-circle subtle">$${cat['add-on-price'].toFixed(0)}</span>
+            <span class="card-author subtle"></span>
+            <h2 class="card-title">${cat.name}</h2>
+            <span class="card-description subtle">${cat.specialSkill}</span>
+            <div class="card-read"><span class="subtle">Color</span>${cat.color}</div>
+          </div>
+          <img src="${cat.gif}" alt="" class="card-media" />
+        </div>`;
+  });
+  $('#cats').html(newString);
+};
+
+getCats()
+  .then((data) => {
+    createCatCards(data.data.cats);
+  })
+  .catch((error) => {
+    console.error({ error });
+  });
+
+$('#cats').show();
